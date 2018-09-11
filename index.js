30835d2ffcd97d2ca9a39d444819f828a2b7aeaa
Chris Castle <castle@heroku.com>
2018-09-10 23:38:27 -0700

    Allow port to be specified in test param

--- index.js
+++ index.js
@@ -9,21 +9,28 @@ const LOCATION = process.env.LOCATION || 'an unknown location'
 app.get('/', (req, res) => res.send(`Hello World from ${LOCATION}!`));
 
 app.get('/test', async (req, res) => {
-  const domain = req.query.domain;
-  if (!domain) return res.status(400).json({'status': 'error', 'message': 'Must specify domain query string parameter.'});
+  const u = req.query.url;
+  if (!u) return res.status(400).json({'status': 'error', 'message': 'Must specify url query string parameter.'});
+
+  let url;
+  try {
+    url = new URL(u);
+  } catch(e) {
+    return res.status(400).json({'status': 'error', 'message': e.message});
+  }
   
   let ipAddress;
   try {
-    ipAddress = await lookup(domain);
+    ipAddress = await lookup(url.hostname);
   } catch {
-    return res.status(200).json({'status': 'ok', 'message': 'Domain not found'});
+    return res.status(200).json({'status': 'ok', 'message': 'IP address not found'});
   }
 
   let fetchResponse;
   try {
-    fetchResponse = await fetch(`http://${domain}/`, { timeout: 2000 });
+    fetchResponse = await fetch(url.href, { timeout: 2000 });
   } catch {
-    return res.status(200).json({'status': 'ok', 'message': 'IP address unreachable'});
+    return res.status(200).json({'status': 'ok', 'message': 'Service unreachable'});
   }
 
   const text = await fetchResponse.text();
@@ -31,7 +38,7 @@ app.get('/test', async (req, res) => {
   if (fetchResponse.ok) {
     return res.status(200).json({'status': 'ok', 'message': text, 'ipAddress': ipAddress})
   } else if (fetchResponse.status === 404) {
-    return res.status(200).json({'status': 'ok', 'message': `No app found at ${domain}`});
+    return res.status(200).json({'status': 'ok', 'message': `No app found at ${url.href}`});
   } else {
     return res.status(500).json({'status': 'error', 'message': 'Unknown error'});
   }
